4b9fb0e96fb7ed79e68c044299f5f3e2e119357f
danish <danishmanu357@gmail.com>
2024-09-24 15:18:25 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -3,23 +3,25 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const taskRoutes = require('./routes/tasks');
+require('dotenv').config();
 
 const app = express();
-const PORT = 5000;
+
+const mongoUri = process.env.MONGODB_URI;
+const port = process.env.PORT || 5000; // Default to port 5000 if not set
 
 // Middleware
 app.use(express.json());
 app.use(cors());
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost/todo-app', { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('Connected to Database'));
+mongoose.connect(mongoUri)
+  .then(() => console.log('Connected to Database'))
+  .catch((error) => console.error('MongoDB connection error:', error));
 
 // Routes
 app.use('/tasks', taskRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
